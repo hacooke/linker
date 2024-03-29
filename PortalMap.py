cc5d178c053c18637d2d249ca3650ac8858a12a3
harry <hcooke006@aol.com>
2024-03-29 10:39:13 +0000

    Moved to RTree implementation for PortalMap
    Changed from QuadTree to RTree implementation, will require libspatialindex as a
    dependency for the project. Implemented first part of link check, getting a list
    of all portals within a linkable range. The second part is simply finding the
    closest portal by euclidean distance; still unsure of the best implementation
    for this -- it would be nice to use the RTree nearest neighbour search for
    efficiency, but if using the entire RTree there is no guarantee that the nearest
    neighbour is in range, so some extra logic is needed. It may be simpler to
    manually calculate the distances for all portals passing the range check but
    there will be instances where this needs to process a very large number of
    portals so may be inefficient. Perhaps the answer is to choose the simple
    implementation and optimise later if required.

--- PortalMap.py
+++ PortalMap.py
@@ -1,43 +1,88 @@
+from rtree import index as rtree
+
+rtree.Index().contains
+
+def CheckDim(dimension)
+    if dimension != "nether" and dimension != "overworld":
+        raise ValueError(f"dimension has value {dimension}, not nether or overworld")
+    return dimension
+
+def IsNether(dimension):
+    CheckDim(dimension)
+    return (dimension == "nether")
+
+def OtherDim(dimension):
+    return "overworld" if IsNether(dimension) else "nether"
+
 class PortalMap:
+    # Portals stored in a list, corresponding index inserted into an R-tree for spatial indexing
     def __init__(self):
-        self.portals=[]
+        # initialise empty portal list
+        self.portals = []
+        # initialise 3D RTrees (1 per dimension)
+        rtree_properties = rtree.Property()
+        rtree_properties.dimension = 3
+        self.nether_tree=rtree.Index(properties=rtree_properties)
+        self.overworld_tree=rtree.Index(properties=rtree_properties)
 
     def AddPortal(self, portal):
+        # take index (length of list before insertion) and add portal to list
+        i = len(self.portals)
         self.portals.append(portal)
+        # add index to tree with bounds of the portal's cube
+        x0, y0, z0 = portal.location
+        x1, y1, z1 = ( loc + size for loc, size in zip(portal.location,portal.size) )
+        self.Tree(portal.dimension).insert(i, (x0, y0, z0, x1, y1, z1))
 
-    def CreateTree(self):
-        # Find min and max x,z coordinate values of all portals
-        extrema = [
-            [
-                func([portal.Extremum(dimension, func) for portal in self.portals])
-                for func in [min,max]
-            ]
-            for dimension in [1,3]
-        ]
-        # convert to widths and central values
-        widths = [ dim_extrema[1]-dim_extrema[0] for dim_extrema in extrema ]
-        central_vals = ( ex[0]+width//2 for ex, width in zip(extrema, widths) )
-        # create tree, pad out width for expansion if needed
-        self.tree = quads.QuadTree( central_vals, widths[0]*1.5. widths[1]*1.5 )
-        # add all portals to tree
-        for portal in self.portals:
-            # for each portal, add all unique x,z coordinates (quadtree only supports points)
-            #...
-            self.tree.insert(quads.Point())
+    def Intersecting(self, p1, p2, dimension):
+        # return a list of indices of portals intersecting the given 3D region(where p1 and p2 are two corners)
+        return list(self.Tree(dimension).intersection((*p1,*p2)))
+        #return [ self.portals[i] for i in self.Tree(dimension).intersection((*p1,*p2)) ]
 
+    def Candidates(self, location, dimension):
+        # return all portals that could be linked to from a given location
+        link_range = 16 if IsNether(dimension) else 128
+        x,_,z = location
+        return self.Intersecting( (x-link_range, -64, z-link_range), (x+link_range, 320, z+link_range), dimension )
+
+    def Tree(self, dimension):
+        return self.nether_tree if IsNether(dimension) else self.overworld_tree
+    
+    def LinkTarget(self, portal):
+        # Find target location:
+        # TODO: get range of locations to cover whole portal range?
+        target_dim = OtherDim(portal.dimension)
+        target_loc = portal.Coordinates(target_dim)
+        # Find portals in link range
+        candidates = self.Candidates(target_loc, target_dim)
+        if len(candidates) == 0: return None
+        if len(candidates) == 1: return self.portals[candidates[0]]
+        # Find closest portals, check they're in range
+        
 
 class Portal:
-    def __init__(self):
-        self.dimension
-        self.bounds #((x1,y1,z1), (x2,y2,z2))
-        self.name
+    def __init__(self, location, size, dimension, name=None):
+        self.location = location #(x,y,z) (where x,y,z are the lowest values in the coordinate range, i.e. bottom left corner)
+        self.size = size #(x_size,y_size,z_size)
+        self.dimension = CheckDim(dimension)
+        self.name = name
 
-    def Extremum(self, dim, func):
-        return func(self.bounds[0][dim], self.bounds[1][dim])
+    def Coordinates(self, dimension):
+        CheckDim(dimension)
+        x,y,z = self.location
+        if dimension == self.dimension: return (x,y,z)
+        if dimension == "overworld": return (x*8, y, z*8)
+        else: return (x//8, y, z//8)
 
-    def Minimum(self, dim):
-        return Extremum(self, dim, min)
+# Example usage
+portal_map = PortalMap()
 
-    def Maximum(self, dim):
-        return Extremum(self, dim, max)
+# Create portals
+portal1 = Portal((0, 0, 0), (1,2,0), 'nether', 'portal1')
+portal2 = Portal((50, 0, 50), (2,2,2), 'overworld', 'portal2')
 
+# Add portals to the map
+portal_map.AddPortal(portal1)
+portal_map.AddPortal(portal2)
+
+print( *[p.name for p in portal_map.Candidates( (16, 10, 16), 'nether' )] )
